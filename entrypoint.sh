0a97f9308ad2783e5a0f8a38c2fa7fbbe14fbe63
Ryan Lee <ryan@parity.io>
2023-03-27 12:13:21 -0400

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-echo "test args $1 $2 $3 $4 $5 $6 $7 $8 $9 $10"
+echo "test args $1 $2 $3 $4 $5 $6 $7 $8 $9 ${$10}"
 
 browser=[$1] || "--browser"
+headless=[$6] || "--headless"
 
-# deno task capi -- deno run -A -r=http://localhost:4646/ https://raw.githubusercontent.com/paritytech/trun/feat/trun-cli/main.ts --dir $1 --importMap $2 --concurrency $3 $browser
+# deno run -A -r=${10} /trun/main.ts --browser-exec-path $2 --concurrency $3 --dir $4 --filter $5 --ignore $7 --importMap $8 --output $9 $browser $headless
