16a00b0d52110a8459ce0816029aa599f9090dd4
Ryan Lee <ryan@parity.io>
2023-03-27 12:28:41 -0400

    a

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,6 @@ echo "test args $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10}"
 
 browser=[$1] || "--browser"
 headless=[$6] || "--headless"
+deno_reload=[${10}] || "-r=${10}"
 
-deno run -A -r=${10} /trun/main.ts --browser-exec-path $2 --concurrency $3 --dir $4 --filter $5 --ignore $7 --importMap $8 --output $9 $browser $headless
+deno run -A -r=${10} /trun/main.ts --browser-exec-path $2 --concurrency $3 --dir $4 --filter $5 --ignore $7 --importMap $8 --output $9 --reload-url=${10} $browser $headless
