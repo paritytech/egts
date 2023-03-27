3d7bc1461339c7b9626c283cafdcb0747f9ed611
Ryan Lee <ryan@parity.io>
2023-03-27 16:31:03 -0400

    a

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,17 +2,59 @@
 
 echo "test args $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10}"
 
-browser=[$1] || "--browser"
-browser_exec_path=[$2] || "--browser-exec-path $2"
-concurrency=[$3] || "--concurrency $3"
-dir=[$4] || "--dir $4"
-filter=[$5] || "--filter $5"
-headless=[$6] || "--headless"
-ignore=[$7] || "--ignore $7"
-import_map=[$8] || "--import-map $8"
-output=[$9] || "--output $9"
-reload_url=[${10}] || "--reload-url ${10}"
-deno_reload=[${10}] || "-r=${10}"
+
+
+browser=""
+if ! [[ -z $1 ]]; then
+  browser="--browser"
+fi
+
+browser_exec_path=""
+if ! [[ -z $2 ]]; then
+  browser_exec_path="--browser-exec-path $2"
+fi
+
+concurrency=""
+if ! [[ -z $3 ]]; then
+  concurrency="--concurrency $3"
+fi
+
+dir=""
+if ! [[ -z $4 ]]; then
+  dir="--dir $4"
+fi
+
+filter=""
+if ! [[ -z $5 ]]; then
+  filter="--filter $5"
+fi
+
+headless=""
+if ! [[ -z $6 ]]; then
+  headless="--headless"
+fi
+
+headless=""
+if ! [[ -z $7 ]]; then
+  ignore="--ignore $7"
+fi
+
+import_map=""
+if ! [[ -z $8 ]]; then
+  import_map="--import-map $8"
+fi
+
+output=""
+if ! [[ -z $9 ]]; then
+  output="--output $9"
+fi
+
+reload_url=""
+deno_reload=""
+if ! [[ -z ${10} ]]; then
+  reload_url="--reload-url ${10}"
+  deno_reload="-r=${10}"
+fi
 
 echo "deno run -A $deno_reload /trun/main.ts $browser $browser_exec_path $concurrency $dir $filter $headless $ignore $import_map $output $reload_url"
 
