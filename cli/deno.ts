288df4aaf02c8817e9124d5a3aab35c586b414b9
Ryan Lee <ryan@parity.io>
2023-05-17 19:20:33 -0400

    fewer

--- cli/deno.ts
+++ cli/deno.ts
@@ -1,95 +1,26 @@
-import { Command } from "../deps/cliffy.ts"
-import { blue, dim, gray, green, red, yellow } from "../deps/std/fmt/colors.ts"
-import { walk } from "../deps/std/fs.ts"
 import { Buffer, readLines } from "../deps/std/io.ts"
-import * as path from "../deps/std/path.ts"
 import { readerFromStreamReader, writeAll } from "../deps/std/streams.ts"
-import { parseFrontmatter } from "../frontmatter.ts"
-import { runWithConcurrency } from "../util.ts"
 
-const cmd = new Command()
-  .name("deno")
-  .option("-c, --concurrency <concurrency:integer>", "concurrency", { default: Infinity })
-  .option("-r, --reload <reload>", "reload")
-  .option("--no-skip", "ignore skip frontmatter")
-  .arguments("<includePatterns..>")
-  .action(async ({ concurrency, skip, reload }, includePatterns) => {
-    const include: string[] = []
-    for await (
-      const { path: pathname } of walk(".", {
-        exts: [".ts", ".tsx"],
-        followSymlinks: true,
-        includeDirs: false,
-        match: includePatterns.split(" ").map((value) => {
-          if (typeof value !== "string") {
-            throw new Error(
-              `Specified an invalid include \`${value}\` (expected a glob or path to example file)`,
-            )
-          }
-          return path.isGlob(value) ? path.globToRegExp(value) : new RegExp(value)
-        }),
-      })
-    ) include.push(pathname)
-
-    const failed: string[] = []
-    let passed = 0
-    let skipped = 0
-
-    await runWithConcurrency(
-      include.map((pathname) => async () => {
-        const { frontmatter } = parseFrontmatter(pathname, await Deno.readTextFile(pathname), {
-          test_skip(value) {
-            return value !== undefined
-          },
-        })
-        const quotedPathname = `"${pathname}"`
-        if (skip && frontmatter.test_skip) {
-          console.log(yellow("Skipped"), quotedPathname)
-          skipped++
-          return
-        }
-        console.log(gray("Testing"), quotedPathname)
-        const logs = new Buffer()
-        const code = await run(pathname, logs, reload)
-        passed++
-        const progress = dim(`(${passed + skipped}/${include.length})`)
-        if (code) {
-          failed.push(pathname)
-          console.log(red("Failed"), progress, quotedPathname)
-          console.log(new TextDecoder().decode(logs.bytes()))
-        } else {
-          console.log(green("Passed"), progress, quotedPathname)
-        }
-      }),
-      concurrency,
-    )
-
-    if (failed.length) {
-      console.log(`${red("Erroring examples")}: \n  - "${failed.join(`"\n  - "`)}"`)
-      Deno.exit(1)
-    } else {
-      if (passed) console.log(blue(`${passed} examples passed`))
-      if (skipped) console.log(gray(`${skipped} examples skipped`))
-      Deno.exit(0)
-    }
-  })
-
-async function run(pathname: string, logs: Buffer, reload?: string): Promise<number> {
-  const flags = reload ? [`-r${reload === "" ? "" : `=${reload}`}`] : []
-  const process = new Deno.Command(Deno.execPath(), {
-    args: ["run", "-A", ...flags, pathname],
-    stdout: "piped",
-    stderr: "piped",
-  }).spawn()
-  const [{ code }] = await Promise.all([
-    process.status,
-    ...[process.stdout, process.stderr].map(async (stream) => {
-      const lineIter = readLines(readerFromStreamReader(stream.getReader()))
-      const encoder = new TextEncoder()
-      for await (const line of lineIter) await writeAll(logs, encoder.encode(`${line}\n`))
-    }),
-  ])
-  return code
+export interface DenoRunFlags {
+  reload?: string | undefined
 }
 
-export default cmd
+export default function run({ reload }: DenoRunFlags) {
+  return async (pathname: string, logs: Buffer): Promise<number> => {
+    const flags = reload ? [`-r${reload === "" ? "" : `=${reload}`}`] : []
+    const process = new Deno.Command(Deno.execPath(), {
+      args: ["run", "-A", ...flags, pathname],
+      stdout: "piped",
+      stderr: "piped",
+    }).spawn()
+    const [{ code }] = await Promise.all([
+      process.status,
+      ...[process.stdout, process.stderr].map(async (stream) => {
+        const lineIter = readLines(readerFromStreamReader(stream.getReader()))
+        const encoder = new TextEncoder()
+        for await (const line of lineIter) await writeAll(logs, encoder.encode(`${line}\n`))
+      }),
+    ])
+    return code
+  }
+}
--- main.ts
+++ main.ts
@@ -1,14 +1,98 @@
 import browser from "./cli/browser.ts"
-import deno from "./cli/deno.ts"
+import runDeno from "./cli/deno.ts"
 import { Command } from "./deps/cliffy.ts"
+import { blue, dim, gray, green, red, yellow } from "./deps/std/fmt/colors.ts"
+import { walk } from "./deps/std/fs.ts"
+import { Buffer } from "./deps/std/io.ts"
+import * as path from "./deps/std/path.ts"
+import { parseFrontmatter } from "./frontmatter.ts"
+import { runWithConcurrency } from "./util.ts"
 
-const test = new Command()
-  .name("test")
-  .command("deno", deno)
-  .command("browser", browser)
+interface GlobalRunnerParams {
+  concurrency: number
+  skip: boolean
+}
+
+type Run<T extends GlobalRunnerParams> = (
+  t: Omit<T, keyof GlobalRunnerParams>,
+) => (pathname: string, logs: Buffer) => Promise<number>
+
+const globalRunner = <T extends GlobalRunnerParams>(f: Run<T>) => {
+  return async ({ concurrency, skip, ...rest }: T, includePatterns: string) => {
+    const include: string[] = []
+    for await (
+      const { path: pathname } of walk(".", {
+        exts: [".ts", ".tsx"],
+        followSymlinks: true,
+        includeDirs: false,
+        match: includePatterns.split(" ").map((value) => {
+          if (typeof value !== "string") {
+            throw new Error(
+              `Specified an invalid include \`${value}\` (expected a glob or path to example file)`,
+            )
+          }
+          return path.isGlob(value) ? path.globToRegExp(value) : new RegExp(value)
+        }),
+      })
+    ) include.push(pathname)
+
+    const failed: string[] = []
+    let passed = 0
+    let skipped = 0
+
+    await runWithConcurrency(
+      include.map((pathname) => async () => {
+        const { frontmatter } = parseFrontmatter(pathname, await Deno.readTextFile(pathname), {
+          test_skip(value) {
+            return value !== undefined
+          },
+        })
+        const quotedPathname = `"${pathname}"`
+        if (skip && frontmatter.test_skip) {
+          console.log(yellow("Skipped"), quotedPathname)
+          skipped++
+          return
+        }
+        console.log(gray("Testing"), quotedPathname)
+        const logs = new Buffer()
+        const code = await f(rest)(pathname, logs)
+        passed++
+        const progress = dim(`(${passed + skipped}/${include.length})`)
+        if (code) {
+          failed.push(pathname)
+          console.log(red("Failed"), progress, quotedPathname)
+          console.log(new TextDecoder().decode(logs.bytes()))
+        } else {
+          console.log(green("Passed"), progress, quotedPathname)
+        }
+      }),
+      concurrency,
+    )
+
+    if (failed.length) {
+      console.log(`${red("Erroring examples")}: \n  - "${failed.join(`"\n  - "`)}"`)
+      Deno.exit(1)
+    } else {
+      if (passed) console.log(blue(`${passed} examples passed`))
+      if (skipped) console.log(gray(`${skipped} examples skipped`))
+      Deno.exit(0)
+    }
+  }
+}
 
 await new Command()
   .name("egts")
   .description("Example-related utilities used in Capi")
-  .command("test", test)
+  .command(
+    "test",
+    new Command()
+      .name("test")
+      .globalOption("-c, --concurrency <concurrency:integer>", "concurrency", { default: Infinity })
+      .globalOption("--no-skip", "ignore skip frontmatter")
+      .command("deno")
+      .arguments("<includePatterns..>")
+      .option("-r, --reload <reload>", "reload")
+      .action(globalRunner(runDeno))
+      .command("browser", browser),
+  )
   .parse(Deno.args)
